221ea3cade37d8858e932b9e2924120542dc501e
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-27 12:11:56 -0700

    exported from Skill Struck

---
+++ reading files.py
@@ -0,0 +1,9 @@
+#Checkpoint
+#file1 = open("file2.txt", "r")
+#print(file1.read())
+#Challenge 1
+#fil = open("cobra.txt", "r")
+#print(fil.read())
+#Challenge 2
+#var = open("speech.txt", "r")
+#print(var.read())
