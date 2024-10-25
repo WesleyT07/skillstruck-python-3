d52c6f5da5d6d06f8763170cc15eea690b24c5da
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-10-25 14:16:25 -0700

    exported from Skill Struck

---
+++ reading parts of a file.py
@@ -0,0 +1,14 @@
+#checkpoint
+#rfile = open("file2.txt", "r")
+#print(rfile.read(10))
+#print(rfile.readline())
+#print(rfile.readline())
+#rfile.close()
+#read a specific
+#filer = open("file2.txt", "r")
+#what = (int(input("how many")))
+#print(filer.read(what))
+#file2 = open("file2.txt", 'r')
+#read = file2.read()
+#read.split()
+#print(len(read))
