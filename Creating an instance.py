caea9a787c4ffb9407a44ab3e0afc12668d402e3
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-11-01 12:11:35 -0700

    exported from Skill Struck

---
+++ Creating an instance.py
@@ -0,0 +1,27 @@
+#class Hat:
+#    def __init__(self, kind, color, material):
+#        self.kind = kind
+#        self.color = color
+#        self.material = material
+#myObject = Hat("brown", "brown", "leather")
+#class Fruit:
+#    def __init__(self, shape, kind, size):
+#        self.shape = shape
+#        self.kind = kind
+#        self.size = size
+#b1 = Fruit("fruit", "fruit", "fruit")
+#b2 = Fruit("fruit", "fruit", "fruit")
+#b3 = Fruit("fruit", "fruit", "fruit")
+#b4 = Fruit("fruit", "fruit", "fruit")
+#print(b1.kind)
+#print(b2.kind)
+#print(b3.kind)
+#print(b4.kind)
+class Pet:
+    def __init__(self, kind, size, legs):
+        self.kind = kind
+        self.size = size
+        self.legs = legs
+p1 = Pet("pet", "pet", "pet")
+p2 = Pet("pet", "pet", "pet")
+p3 = Pet("pet", "pet", "pet")
