099b4899d105e22b33f50371bd0242ce104dc7a2
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-11-01 12:11:34 -0700

    exported from Skill Struck

---
+++ object oriented programming.py
@@ -0,0 +1,17 @@
+#class Dog:
+#    def __init__(self, name, color, size):
+#        self.name = name
+#        self.color = color
+#        self.size = size
+#class Car:
+#    def __init__(self, kind, color, year):
+#        self.kind = kind
+#        self.color = color
+#        self.year = year
+class Tree:
+    def __init__(self, fruit, height, leaf, flower, seed):
+        self.fruit = fruit
+        self.height = height
+        self.leaf = leaf
+        self.flower = flower
+        self.seed = seed
