e935379658f8dfded964bc338ae215a1dd654892
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-11-08 12:36:37 -0700

    exported from Skill Struck

---
+++ recursion vs iteration.py
@@ -0,0 +1,29 @@
+#inp = int(input("number:"))
+#def factorial(n): 
+#	if n <= 1:
+#		return 1
+#	else:
+#		return (n * factorial(n - 1))
+#print(factorial(inp))
+#Add the list
+#my_list = [int(n) for n in input("numbers").split()]
+#def add_list(my_list):
+#	if len(my_list) == 1:
+#		return my_list[0]
+#	else:
+#		return my_list[0] + add_list(my_list[1:])
+#print(add_list(my_list))
+#Fibonacci
+#def fibonacci(n):
+#    if n <= 1:
+#        return n
+#    else:
+#        return fibonacci(n - 1) + fibonacci(n - 2)
+#def generate_fibonacci_sequence(count):
+#    fibonacci_sequence = []
+#    for i in range(count):
+#        fibonacci_sequence.append(fibonacci(i))
+#    return fibonacci_sequence
+#num = int(input("Enter the number of Fibonacci sequence terms: "))
+#fibonacci_sequence = generate_fibonacci_sequence(num)
+#print(fibonacci_sequence)
