16c306530b923128344188c548100bb91ac07530
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-11-08 12:36:35 -0700

    exported from Skill Struck

---
+++ STACKS.py
@@ -0,0 +1,20 @@
+#stack = []
+#stack.append("w")
+#stack.append("w")
+#stack.append("w")
+#stack.append("w")
+#stack.pop()
+#print(stack)
+#answer = ["apples", "steak", "potatoes", "carrots"]
+#food = input("food w/ s").lower()
+#if "s" in food:
+#    answer.append(food)
+#    print(answer)
+#else:
+#    print("The input doesn't have the letter s")
+answer = ["c", "r", "o", "a", "k"]
+answer.pop()
+answer.pop()
+answer.append("p")
+answer.append("s")
+print(answer)
