d0eeeab1a4e9801fbf7dbe32a13dc28d48609480
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-11-08 12:36:37 -0700

    exported from Skill Struck

---
+++ bubble sort.py
@@ -0,0 +1,11 @@
+#arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+#inp = int(input("Enter a number"))
+#dog = arr.append(inp)
+#def BubbleSort(list):
+    #n = len(list)
+    #for i in range(n):
+        #for j in range(0, n - i - 1):
+            #if list[j] > list[j + 1]:
+                #list[j], list[j + 1] = list[j + 1], list[j]
+    #return list
+#print(BubbleSort(arr))
