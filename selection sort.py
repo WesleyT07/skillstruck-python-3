9060c00a429b55c5fa3b781ce5ee387f8b81bb2b
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-11-08 12:36:38 -0700

    exported from Skill Struck

---
+++ selection sort.py
@@ -0,0 +1,13 @@
+#arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+#inp = int(input("number:"))
+#arr.append(inp)
+#def SelectionSort(lst):
+#    for i in range(len(lst)):
+#        minimum = i
+#        for j in range(i + 1, len(lst)):
+#            if lst[j] < lst[minimum]:
+#                minimum = j
+#        if minimum != i:
+#            lst[i], lst[minimum] = lst[minimum], lst[i]
+#    return lst
+#print(SelectionSort(arr))
